9a1095a0f93cf53315f10c7e3c5775b61bac9576
Curtis McEnroe <programble@gmail.com>
2013-08-03 18:51:13 -0400

    Parse anything that MPD throws at us (hopefully)

--- server.js
+++ server.js
@@ -12,14 +12,29 @@ var _ = require('underscore'),
 
 // MPD Parsing //
 
-mpd.parseObject = function(data) {
-  var obj = {};
+// FIXME: Be less hacky thanks
+mpd.parse = function(data) {
+  var parsed = [],
+      obj = {};
   data.split('\n').forEach(function(line) {
     var sep = line.indexOf(': ');
-    if (sep != -1)
-      obj[line[0].toLowerCase() + line.slice(1, sep)] = line.slice(sep + 2);
+    if (sep != -1) {
+      var key = line[0].toLowerCase() + line.slice(1, sep),
+          val = line.slice(sep + 2);
+      if (obj[key]) {
+        if (_.size(obj) == 1) obj = obj[key];
+        parsed.push(obj);
+        obj = {};
+      }
+      obj[key] = val;
+    }
   });
-  return obj;
+  if (_.size(parsed)) {
+    parsed.push((_.size(obj) == 1) ? _.values(obj)[0] : obj);
+    return parsed;
+  } else {
+    return obj;
+  }
 }
 
 // The MPD lib actually sucks so here are some convenience methods
@@ -29,7 +44,7 @@ mpd.prototype.cmd = function() {
       fn = _.last(params),
       args = _.rest(_.initial(params));
   this.sendCommand(mpd.cmd(cmd, args), function(err, data) {
-    if (_.isFunction(fn)) fn(err, err ? data : mpd.parseObject(data));
+    if (_.isFunction(fn)) fn(err, err ? data : mpd.parse(data));
   });
 }
 
