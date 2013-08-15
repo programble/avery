6e1f168f7e959bee4900978970e0de0da189da9a
Curtis McEnroe <programble@gmail.com>
2013-08-15 01:15:32 -0400

    Render favicons once page is loaded

--- client/favicon.js
+++ client/favicon.js
@@ -1,19 +1,23 @@
-function renderIcon(text) {
-  var canvas = document.createElement('canvas'),
-      ctx = canvas.getContext('2d');
-  canvas.width = canvas.height = 16;
-  ctx.fillStyle = '#222';
-  ctx.font = '12px FontAwesome';
-  ctx.fillText(text, 3, 12);
-  return canvas.toDataURL('image/png');
-}
-
-var stopFavicon = renderIcon('\uf001'),
-    playFavicon = renderIcon('\uf04b'),
-    pauseFavicon = renderIcon('\uf04c');
+var stopFavicon, playFavicon, pauseFavicon;
 
 function favicon(icon) {
   $('#favicon').attr('href', icon);
 }
 
-favicon(stopFavicon);
+$(function() {
+  function renderIcon(text) {
+    var canvas = document.createElement('canvas'),
+        ctx = canvas.getContext('2d');
+    canvas.width = canvas.height = 16;
+    ctx.fillStyle = '#222';
+    ctx.font = '12px FontAwesome';
+    ctx.fillText(text, 3, 12);
+    return canvas.toDataURL('image/png');
+  }
+
+  stopFavicon = renderIcon('\uf001');
+  playFavicon = renderIcon('\uf04b');
+  pauseFavicon = renderIcon('\uf04c');
+
+  favicon(stopFavicon);
+});
